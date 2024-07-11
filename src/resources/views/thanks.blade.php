a2f498855de5a1bc1a94fbd5ecf16b1c2399f23d
yanagi <liuguzaoji@gmail.com>
2024-07-11 23:30:26 +0900

    サンクスページ入力

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asser('css/thanks.css') }}">
+@endsection
+
+@section('content')
+<div class="thanks__content">
+    <div class="thanks__backtext">Thank you</div>
+    <div class="thanks__heading">
+        <h2>お問い合わせありがとうございました</h2>
+        
+    </div>
+</div>
+@endsection
