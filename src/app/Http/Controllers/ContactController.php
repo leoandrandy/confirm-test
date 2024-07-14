df5b86e1caf2597698f9f009e48e83ce97b090db
yanagi <liuguzaoji@gmail.com>
2024-07-14 13:38:48 +0900

    バリデーション記入

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -12,13 +12,13 @@ class ContactController extends Controller
         return view('index');
     }
 
-    public function confirm(Request $request)
+    public function confirm(ContactRequest $request)
     {
         $contact = $request->only(['first_name', 'last_name', 'gender', 'email', 'tel_1', 'tel_2', 'tel_3', 'address', 'building', 'category', 'detail']);
         return view('confirm', compact('contact'));
     }
 
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
         $contact = (['fullname' => $request->first_name . $request->last_name] + ['tell' => $request->tel_1 . $request->tel_2 . $request->tel_3] + $request->only(['gender', 'email', 'address', 'building', 'category', 'detail']));
 
--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -26,7 +26,42 @@ class ContactRequest extends FormRequest
         return [
             'first_name' => ['required', 'string'],
             'last_name' => ['required', 'string'],
-            
+            'gender' => ['required'],
+            'email' => ['required', 'string', 'email'],
+            'tel_1' => ['required', 'integer', 'max:5'],
+            'tel_2' => ['required', 'integer', 'max:5'],
+            'tel_3' => ['required', 'integer', 'max:5'],  
+            'address' => ['required', 'string'],
+            'category' => ['required'],
+            'detail' => ['required', 'max:120'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'first_name.required' => '姓を入力してください',
+            'first_name.string' => '姓を文字列で入力してください',
+            'last_name.required' => '名を入力してください',
+            'last_name.string' => '名を文字列で入力してください',
+            'gender.required' => '性別を選択してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.string' => 'メールアドレスを文字列で入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'tel_1.required' => '電話番号を入力してください',
+            'tel_1.integer' => '電話番号を数値で入力してください',
+            'tel_1.max:5' => '電話番号は各欄5桁までの数値で入力してください',
+            'tel_2.required' => '電話番号を入力してください',
+            'tel_2.integer' => '電話番号を数値で入力してください',
+            'tel_2.max:5' => '電話番号は各欄5桁までの数値で入力してください',
+            'tel_3.required' => '電話番号を入力してください',
+            'tel_3.integer' => '電話番号を数値で入力してください',
+            'tel_3.max:5' => '電話番号は各欄5桁までの数字で入力してください',
+            'address.required' => '住所を入力してください',
+            'address.string' => '住所を文字列で入力してください',
+            'category.required' => 'お問い合わせの種類を選択してください',
+            'detail.required' => 'お問い合わせ内容を入力してください',
+            'detail.max:120' => 'お問い合わせ内容は120文字以内で入力してください',
         ];
     }
 
